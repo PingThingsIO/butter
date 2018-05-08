49facf2ed76e03c145aa2df9acd434585caaed35
cameron <emptyflash@gmail.com>
2018-05-07 22:09:14 -0500

    Switch to use mow.cli and add copy command

---
+++ README.md
@@ -0,0 +1,7 @@
+```
+go get
+go install
+butter cp example.com:4410 localhost:4410 -s 2017-12-10T15:04:05+07:00 -e 2017-12-11T15:04:05+07:00 pingthingsio/90807,pingthingsio/90807,name=L1MAG
+butter ls
+butter rm 0754ff2e-7770-4916-ab64-b57e530454e5
+```
--- main.go
+++ main.go
@@ -5,73 +5,82 @@ import (
 	"os"
 	"strings"
 
-	"github.com/urfave/cli"
+	"github.com/jawher/mow.cli"
 )
 
-func listCommand(c *cli.Context) error {
-	// Hack for positional arguments https://github.com/urfave/cli/pull/140#issuecomment-131841364
-	cli.CommandHelpTemplate = strings.Replace(cli.CommandHelpTemplate, "[arguments...]", "[endpoint (default: localhost:4410)]", -1)
-	endpoint := c.Args().First()
-	if endpoint == "" {
-		endpoint = "localhost:4410"
+func listCommand(cmd *cli.Cmd) {
+	cmd.Spec = "[ENDPOINT] [PREFIX]"
+	endpoint := cmd.StringArg("ENDPOINT", "localhost:4410", "The BTrDB endpoint to list")
+	prefix := cmd.StringArg("PREFIX", "", "A prefix to filter collections.")
+	cmd.Action = func() {
+		list(*endpoint, *prefix)
 	}
+}
 
-	list(endpoint, c.String("prefix"))
+func removeCommand(cmd *cli.Cmd) {
+	cmd.Spec = "[ENDPOINT] UUID [-y]"
+	endpoint := cmd.StringArg("ENDPOINT", "localhost:4410", "The BTrDB endpoint to list")
+	uuid := cmd.StringArg("UUID", "", "UUID of the stream to delete")
+	confirmed := cmd.BoolOpt("y yes", false, "Skip confirmation prompt")
+	cmd.Action = func() {
+		remove(*endpoint, *uuid, *confirmed)
+	}
+}
+
+type StreamConfigs []StreamConfig
+
+func (s *StreamConfigs) Set(v string) error {
+	parts := strings.Split(v, ",")
+	if len(parts) < 2 {
+		return fmt.Errorf("Stream config requires src_collection and dest_collection, %v is invalid", v)
+	}
+	stream := StreamConfig{}
+	stream.Tags = make(map[string]string)
+	stream.SrcCollection = parts[0]
+	stream.DstCollection = parts[1]
+	tags := parts[2:len(parts)]
+	for _, tag := range tags {
+		tagParts := strings.Split(tag, "=")
+		if len(tagParts) < 2 {
+			return fmt.Errorf("Tag must follow format tagname=tagvalue, %v is invalid", tag)
+		}
+		tagName := tagParts[0]
+		tagValue := tagParts[1]
+		stream.Tags[tagName] = tagValue
+	}
+	*s = append(*s, stream)
 	return nil
 }
 
-func removeCommand(c *cli.Context) error {
-	cli.CommandHelpTemplate = strings.Replace(cli.CommandHelpTemplate, "[arguments...]", "[endpoint] [stream uuid]", -1)
+func (s *StreamConfigs) String() string {
+	return fmt.Sprintf("%v", *s)
+}
 
-	var endpoint string
-	var uuid string
-	if c.NArg() == 1 {
-		uuid = c.Args().Get(0)
-		endpoint = "localhost:4410"
-	} else {
-		uuid = c.Args().Get(1)
-		endpoint = c.Args().First()
+func (s *StreamConfigs) Clear() {
+	*s = []StreamConfig{}
+}
+
+func copyCommand(cmd *cli.Cmd) {
+	cmd.Spec = "FROMSERVER TOSERVER [-sea] STREAMCONFIG..."
+	fromServer := cmd.StringArg("FROMSERVER", "", "BTrDB endpoint to copy from")
+	toServer := cmd.StringArg("TOSERVER", "", "BTrDB endpoint to copy to")
+	start := cmd.StringOpt("s start", "", "Start time of the range to copy (in format 2006-01-02T15:04:05+07:00)")
+	end := cmd.StringOpt("e end", "", "End time of the range to copy (in format 2006-01-02T15:04:05+07:00)")
+	abortIfExists := cmd.BoolOpt("a abort", false, "Abort the copy if the collection already exists")
+	streamCfg := &StreamConfigs{}
+	cmd.VarArg("STREAMCONFIG", streamCfg, "Config for the streams to copy (follows the format src_collection,dest_collection,tagname=tagvalue)")
+	cmd.Action = func() {
+		cfg := Config{*fromServer, *toServer, *start, *end, *abortIfExists, *streamCfg}
+		cp(cfg)
 	}
-
-	if uuid == "" {
-		fmt.Println("stream uuid positional argument is required")
-		cli.ShowCommandHelp(c, "rm")
-		os.Exit(1)
-	}
-
-	remove(endpoint, uuid, c.Bool("yes"))
-	return nil
 }
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "butter"
-	app.Usage = "Useful BTrDB CLI tools for development"
+	app := cli.App("butter", "Useful BTrDB CLI tools for development")
 
-	app.Commands = []cli.Command{
-		{
-			Name:  "ls",
-			Usage: "List collections for a BTrDB endpoint. If only one collection is returned, its streams will be listed.",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "prefix,p",
-					Usage: "Prefix to filter collection names",
-				},
-			},
-			Action: listCommand,
-		},
-		{
-			Name:  "rm",
-			Usage: "Remove a stream from BTrDB",
-			Flags: []cli.Flag{
-				cli.BoolFlag{
-					Name:  "yes,y",
-					Usage: "Skips confirmation prompt",
-				},
-			},
-			Action: removeCommand,
-		},
-	}
+	app.Command("ls", "List collections for a BTrDB endpoint. If only one collection is returned, its streams will be listed.", listCommand)
+	app.Command("rm", "Remove a stream from BTrDB", removeCommand)
+	app.Command("cp", "Copy a collection from one BTrDB server to another", copyCommand)
 
 	err := app.Run(os.Args)
 	if err != nil {
